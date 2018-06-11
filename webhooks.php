471d59766ab5e704e354c3801ee1483132b51234
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-11 22:14:59 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,41 +1,53 @@
 <?php
-
- date_default_timezone_set('Asia/Bangkok');
-
-$servername ="localhost";
-$username = "root";
-$password = "12345678";
-$dbname = "arduino";
-$now = new DateTime();
-
-$temp = $_REQUEST['temp'];
-$humidity = $_REQUEST['humidity'];
-
-
-$conn = mysqli_connect("localhost","root","");
-if (!$conn)
-{
-    die('Could not connect: ' . mysqli_error());
+ 
+$strAccessToken = "ACCESS_TOKEN";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+ }else if($arrJson['events'][0]['message']['text'] == "แอม"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "มะไหร่จะผอม";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-//$con_result = mysqli_select_db($dbname);
-$con_result = mysqli_select_db($conn, $dbname) or die(mysqli_error($con));
-if(!$con_result)
-{
- die('Could not connect to specific database: ' . mysqli_error());
-}
-
- $datenow = $now->format("Y-m-d H:i:s");
- $hvalue = $value;
-
- $sql ="insert  into  temp  (id,temp,humidity,date) values ( null,$temp,$humidity,'$datenow')";
-// echo $sql;
-
-
- $result = mysqli_query($conn, $sql);
- if (!$result) {
-  die('Invalid query: ' . mysqli_error());
- }
- echo "<h1 align=center>THE DATA HAS BEEN SENT!!</h1>";
- mysqli_close($conn);
-
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
 ?>
