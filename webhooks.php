e15e6917e2fed71aef53aa6b2637175700894447
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-11 22:04:59 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,45 +1,41 @@
-<?php // callback.php
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = 'sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=';
+<?php
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['userId'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $text
-];
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
+ date_default_timezone_set('Asia/Bangkok');
+
+$servername ="localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "arduino";
+$now = new DateTime();
+
+$temp = $_REQUEST['temp'];
+$humidity = $_REQUEST['humidity'];
+
+
+$conn = mysqli_connect("localhost","root","");
+if (!$conn)
+{
+    die('Could not connect: ' . mysqli_error());
 }
+//$con_result = mysqli_select_db($dbname);
+$con_result = mysqli_select_db($conn, $dbname) or die(mysqli_error($con));
+if(!$con_result)
+{
+ die('Could not connect to specific database: ' . mysqli_error());
 }
-}
-echo "OK";
+
+ $datenow = $now->format("Y-m-d H:i:s");
+ $hvalue = $value;
+
+ $sql ="insert  into  temp  (id,temp,humidity,date) values ( null,$temp,$humidity,'$datenow')";
+// echo $sql;
+
+
+ $result = mysqli_query($conn, $sql);
+ if (!$result) {
+  die('Invalid query: ' . mysqli_error());
+ }
+ echo "<h1 align=center>THE DATA HAS BEEN SENT!!</h1>";
+ mysqli_close($conn);
+
+?>
