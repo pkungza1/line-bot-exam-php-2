4d9bcff65fdaf9150bae6fe73f6992a6ae6488c6
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-13 23:55:51 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,42 +1,72 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
 <head>
-<title>kitti</title>
-</head>
-<body>
-<?php
-$servername ="localhost";
-$username = "root";
-$password = "";
-$dbname = "arduino";
-$objConnect = mysqli_connect($servername,$username,$password,$dbname) or die("Error Connect to Database");
-$SQL = "SELECT * FROM temp";
-$objQuery = mysqli_query($objConnect,$SQL) or die ("Error Query [".$strSQL."]");
-?>
-<table width="600" border="1" align="center">
-  <tr>
-    <th width="91"> <div align="center">ID </div></th>
-    <th width="98"> <div align="center">TEMP </div></th>
- <th width="98"> <div align="center">Humidity </div></th>
-    <th width="198"> <div align="center">DATE </div></th>
+
+  <title>Google Gauge - ThingSpeak</title>
+<style type="text/css">
+  body { background-color: #ffffff; }
+</style>
   
-  </tr>
-<?php
-while($objResult = mysqli_fetch_array($objQuery))
-{
-?>
-  <tr>
-    <td><div align="center"><?php echo $objResult["id"];?></div></td>
-    <td align="center"><?php echo $objResult["temp"];?></td>
- <td align="center"><?php echo $objResult["humidity"];?></td>
-    <td align="center"><?php echo $objResult["date"];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($objConnect);
-?>
-</body>
-</html>
+<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js'></script> 
+<script type='text/javascript' src='https://www.google.com/jsapi'></script>
+<script type='text/javascript'>
+
+ var chart; 
+ var charts;
+ var data;
+
+       google.load('visualization', '1', {packages:['gauge']});
+       google.setOnLoadCallback(initChart);
+
+ function displayData(point) {
+ 
+  data.setValue(0, 0, 'Temp Dr');
+  data.setValue(0, 1, point);
+  chart.draw(data, options);
+ 
+ }
+
+ function loadData() {
+ 
+  // variable for the data point
+  var p;
+ 
+               // ให้เปลี่ยนตัวเลข เป็น channel thingspeak เป็นของตัวเอง
+  $.getJSON('https://api.thingspeak.com/channels/516462/feed/last.json?callback=?', function(data) {
+  
+  // get the data point
+  p = data.field1;
+ 
+  if (p)
+  {
+   //p = Math.round((p / 1023) * 100);
+   displayData(p);
+  }
+ 
+  });
+ 
+ }
+
+ function initChart() {
+
+  data = new google.visualization.DataTable();
+  data.addColumn('string', 'Label');
+  data.addColumn('number', 'Value');
+  data.addRows(1);
+         
+         chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+         options = {width: 320, height: 320, redFrom: 40, redTo: 100,
+             yellowFrom:25, yellowTo: 40, minorTicks: 5};
+ 
+  loadData();
+
+  setInterval('loadData()', 1500);
+         
+ }
+
+</script>
+  <body>
+    <div id='chart_div' align="center"></div>
+  </body>
+</html> 
+</HTML>
