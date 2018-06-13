bad21fc4ce792a443254068e6d8189ada9c6b759
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-13 22:31:53 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,36 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tutor";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "SELECT * FROM esp8266 ORDER by id DESC LIMIT 5";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-	echo "<table border='1'><th>ID</th><th>TEMP</th><th>DATETIME</th>";
-    while($row = $result->fetch_assoc()) {
-		echo "<tr>";
-		echo "<td>".$row['id']."</td>";
-		echo "<td>".$row['temp']."</td>";
-		echo "<td>".$row['day']."</td>";
-		echo "</tr>";
-    }
-	echo "</table>";
-} else {
-    echo "0 results";
-}
-$conn->close();
-
-
-
 $strAccessToken = "sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
