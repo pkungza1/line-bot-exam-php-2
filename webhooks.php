6152d52a167550d40c3a0d48023e60d4c1483e0e
pkungza1 <40157840+pkungza1@users.noreply.github.com>
2018-06-14 09:00:04 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,4 +1,10 @@
 <?php
+define('LINE_MESSAGE_CHANNEL_ID','1585854926');
+define('LINE_MESSAGE_CHANNEL_SECRET','972eda038e3b3d879d3f3edbb9c314ed');
+define('LINE_MESSAGE_ACCESS_TOKEN','sSDv7p584jZ76zRoS3qc1n2VgpPeCRWlIsnYKAiYMR8xcWYPtsMjCxMQiegFygBAMiRRxoNuIUlC5I+sGTTShnJiV637tpjiiotwgvgIQKNLan+rfsTb/wl13EiLqUBmgNvv17qw6B2pnt8Xj/LxjAdB04t89/1O/w1cDnyilFU=');
+
+
+
 // กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
